2223098d4feb0688fb3f52b8f73bd186e99edaf4
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2020-10-24 13:57:25 +0200

    Updates

---
+++ precache-manifest.7cc3ba0caf3546f842276f0eedb9d600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c1f4357a5498a38665fe9678059730",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "016fcf34590820680734",
+    "url": "/Portfolio/static/css/2.59f89186.chunk.css"
+  },
+  {
+    "revision": "49d0352c46aa25721d61",
+    "url": "/Portfolio/static/css/main.85c0fc5c.chunk.css"
+  },
+  {
+    "revision": "016fcf34590820680734",
+    "url": "/Portfolio/static/js/2.5690d5ad.chunk.js"
+  },
+  {
+    "revision": "87f1c48a6b299dccc37ab9301d949db7",
+    "url": "/Portfolio/static/js/2.5690d5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d0352c46aa25721d61",
+    "url": "/Portfolio/static/js/main.271a1350.chunk.js"
+  },
+  {
+    "revision": "a58fb71f5f65d20e2e2e",
+    "url": "/Portfolio/static/js/runtime-main.c378073a.js"
+  }
+]);
